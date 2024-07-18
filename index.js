6ef13055007bc8a7866b4e8a92a179149682d29c
Mark Brian <markbkiunga2017@gmail.com>
2024-07-18 11:35:15 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`Wrapped ${array[i]} and added a bow!`);
+    debugger;
+  }
+}
+
+// wrapGifts(gifts);
+
+function writeCards(array, eventName) {
+  let thankYouMessages = [];
+  for (let i = 0; i < array.length; i++) {
+    const cardMessage = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+    thankYouMessages.push(cardMessage);
+    console.log(cardMessage);
+  }
+  console.log(thankYouMessages);
+  return thankYouMessages;
+}
+let peopleToThank = ['Guadalupe', 'Ollie', 'Aki'];
+// writeCards(peopleToThank, 'birthday');
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
+countDown(10);
